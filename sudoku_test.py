46b2cdc7a40bf10b2f0b67d91656c577e59f803e
tking8 <54691809+tking8@users.noreply.github.com>
2020-09-20 13:36:19 -0700

    Add files via upload

---
+++ sudoku_test.py
@@ -0,0 +1,10 @@
+from sudoku import *
+asudoku=sudoku()
+asudoku.generate()
+asudoku.display()
+asudoku=sudoku()
+asudoku.makepuzzle(40)
+asudoku.display()
+asudoku.solved()
+asudoku.solve()
+asudoku.display()
